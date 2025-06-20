4f1a48ca9c3cbb2450fa3e4c0e86e944f7c750fb
Carlos Villarreal <carlos.villarreal01@epn.edu.ec>
2025-06-20 08:46:04 -0500

    docker-compose

--- calculadora/src/app.controller.ts
+++ calculadora/src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('database')
+  getDatabaseConnection() {
+    return {
+      DB_HOST: this.configService.get('DB_HOST'),
+      DB_PASSWORD: this.configService.get('DB_PASSWORD'),
+      DB_NAME: this.configService.get('DB_NAME'),
+      DB_USER: process.env.DB_USER,
+    };
   }
 }
